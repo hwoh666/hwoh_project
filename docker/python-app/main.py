1e2a7782f2677b229383601021eff3d9f2c6130e
hwoh <hwoh666@gmail.com>
2022-10-04 22:38:36 +0400

    add docker train

---
+++ docker/python-app/main.py
@@ -0,0 +1,10 @@
+import calendar
+
+print ('Добро пожаловать в super календарь \n')
+
+year = int(input('Пожалуйста введите год:'))
+month = int(input ('Введите номер месяца: '))
+
+print(calendar.month(year, month))
+
+print('Иди нахуй (всего хорошего)')
--- main.py
+++ main.py
@@ -1,4 +1,47 @@
-def main():
-    print("putin eat more shit")
-    
-main()
+from ipaddress import ip_address
+from pickle import TRUE
+from random import vonmisesvariate
+from tkinter import X
+from xmlrpc.client import TRANSPORT_ERROR
+
+
+print("putin eat more shit")
+#putin = "vor"    
+#hwoh = 666
+#print(putin, hwoh)
+# inp = input()
+# print(inp + " " + putin)
+#x = 2
+#y = 3
+#print (f'{x}{y}')
+#print(x + y)
+# при запуски программы ввожу любую строку, вывести должна "Hello, ____ !"
+#input = input()
+#print("Hello, " + input + "!")
+#print(f"Hello, {input}!" )
+# 1. Создать две переменные (числа) вывести сумму, разность, произведение и
+# что нибудь разделить и корень из этих  чисел, отсаток от деления.
+# 2. Числа должны вводится в командной строке.
+# print("vvedi pervoe chislo hmir`") 
+# x = float(input())
+# print("vvedi vtoroe chislo hmir`")
+# y = float(input())
+# print(f"summma { x + y }")
+# print(f"raznost { x - y }")
+# print(f"delenie { x / y }")
+# print(f"umnogit { x * y }")
+# print(f"koren iz { x ** (0.5) } {y ** (0.5)}")
+# print(f"ostatok delenie { x % y }")
+
+
+# если число четное вывести принт "четное" если число не четное "вывести нечетное"
+while True:
+    print("Введи число")
+    y = input()
+    if y == "exit":
+        break
+    x = float(y)
+    if x % 2 == 0:
+        print("Четное")
+    else:
+        print("Нечетное")
